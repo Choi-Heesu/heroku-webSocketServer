e60af1ed94637bf43d4b4b37b7c4f0ccf8c03482
heesu_choi <chlgmltn101@naver.com>
2020-11-27 17:25:48 +0900

    Add Default Web Socket Server

---
+++ socket.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT;
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http, {
+  origin: '*'
+});
+
+io.on('connection', function (socket) {
+  console.log('websocket connected');
+  socket.on('blob', function (data) {
+    io.emit('return', data);
+    console.log(data);
+  });
+
+  socket.on('disconnect', function () {
+    console.log("socket disconnected!");
+  });
+});
+
+http.listen(PORT, function () {
+  console.log('backend listening on *:80');
+});
