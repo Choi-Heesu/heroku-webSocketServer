3b1b77dda1d754b6aef9026de141cc018992fcef
heesu_choi <chlgmltn101@naver.com>
2020-11-30 15:42:00 +0900

    Testing Origins

--- socket.js
+++ socket.js
@@ -28,7 +28,7 @@ setInterval(function () {
 var httpServer = http.Server(app);
 var io = require('socket.io')(httpServer, {
   cors: {
-    origin: "https://192.168.9.90",
+    origin: ["https://192.168.9.90:3000", "https://192.168.9.90:5000"],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
