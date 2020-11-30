0de52cd59637cb0cc257912165d2dc2735e7ee6b
heesu_choi <chlgmltn101@naver.com>
2020-11-30 12:32:02 +0900

    Testing Cors

--- socket.js
+++ socket.js
@@ -25,14 +25,9 @@ setInterval(function () {
 
 var httpServer = http.Server(app);
 var io = require('socket.io')(httpServer, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-      "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
+  cors: {
+    origin: "*",
+    credentials: true
   }
 });
 
